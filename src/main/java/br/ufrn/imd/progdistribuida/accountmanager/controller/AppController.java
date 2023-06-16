8830e067b0cdb917aa5b6cf5828e83ff45b4b754
Dimitri <dmitrilima56@gmail.com>
2023-06-16 02:28:09 -0300

    crud dos aplicativos e criação do controller

---
+++ src/main/java/br/ufrn/imd/progdistribuida/accountmanager/controller/AppController.java
@@ -0,0 +1,43 @@
+package br.ufrn.imd.progdistribuida.accountmanager.controller;
+
+import br.ufrn.imd.progdistribuida.accountmanager.model.App;
+import br.ufrn.imd.progdistribuida.accountmanager.service.AppService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/apps")
+@RequiredArgsConstructor
+public class AppController {
+
+    private final AppService appService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<List<App>> findAppsByUserId(@PathVariable String userId) {
+        List<App> apps = appService.findAppsByUserId(userId);
+        return ResponseEntity.ok(apps);
+    }
+
+    @PostMapping
+    public ResponseEntity<App> createApp(@RequestBody App app) {
+        App createdApp = appService.createApp(app);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdApp);
+    }
+
+    @PutMapping("/{appId}")
+    public ResponseEntity<App> updateApp(@PathVariable Long appId, @RequestBody App app) {
+        app.setId(appId);
+        App updatedApp = appService.updateApp(app);
+        return ResponseEntity.ok(updatedApp);
+    }
+
+    @DeleteMapping("/{appId}")
+    public ResponseEntity<Void> deleteApp(@PathVariable Long appId) {
+        appService.deleteApp(appId);
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/br/ufrn/imd/progdistribuida/accountmanager/service/AppService.java
+++ src/main/java/br/ufrn/imd/progdistribuida/accountmanager/service/AppService.java
@@ -16,4 +16,26 @@ public class AppService {
     public List<App> findAppsByUserId(String userId){
         return appRepository.findAppByUserId(userId);
     }
+
+    public App createApp(App app) {
+        return appRepository.save(app);
+    }
+
+    public App updateApp(App app) {
+
+        if (appRepository.existsById(app.getId())) {
+            return appRepository.save(app);
+        } else {
+            throw new IllegalArgumentException("O aplicativo não existe.");
+        }
+    }
+
+    public void deleteApp(Long appId) {
+
+        if (appRepository.existsById(appId)) {
+            appRepository.deleteById(appId);
+        } else {
+            throw new IllegalArgumentException("O aplicativo não existe.");
+        }
+    }
 }
